4f6c1ce66dcbfeae065367e629b6923c74ffedd9
acme1982 <mariusnalivaika@Mariuss-MacBook-Pro.local>
2020-01-03 12:10:59 +0000

    Changed onSearchSubmit funcion to return number of images found

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,22 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
+	state = { images: [] };
+	onSearchSubmit = async term => {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization:
 					'Client-ID b070997e9eeec4d5874fac68ae9604a20327f376f87ce5cf7eb7bac0e1e17f00'
 			}
 		});
-	}
+		this.setState({ images: response.data.results });
+	};
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				Found: {this.state.images.length} Images
 			</div>
 		);
 	}
