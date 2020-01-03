81b61e4c2789f257f36073e1bd2414ac29003445
acme1982 <mariusnalivaika@Mariuss-MacBook-Pro.local>
2020-01-03 11:43:14 +0000

    Added axios to get data from remote API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization:
+					'Client-ID b070997e9eeec4d5874fac68ae9604a20327f376f87ce5cf7eb7bac0e1e17f00'
+			}
+		});
 	}
 	render() {
 		return (
